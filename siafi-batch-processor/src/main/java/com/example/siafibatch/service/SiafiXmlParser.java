8c606b5c2937af990471c4e01c3f18f9a873fb1d
joelmaykon <joelmaykon94@gmail.com>
2026-07-12 00:46:39 -0300

    refactor: apply SOLID and KISS to SiafiXmlParser spliting parse elements into sub-methods

--- siafi-batch-processor/src/main/java/com/example/siafibatch/service/SiafiXmlParser.java
+++ siafi-batch-processor/src/main/java/com/example/siafibatch/service/SiafiXmlParser.java
@@ -26,73 +26,72 @@ public class SiafiXmlParser {
         Document doc = builder.parse(inputStream);
         doc.getDocumentElement().normalize();
 
-        // 1. Get header information
+        Element headerEl = getHeaderElement(doc);
+        String codigoLayout = getTagValue(headerEl, "codigoLayout");
+
+        if ("DH001".equals(codigoLayout)) {
+            return buildDhCarga(doc, headerEl, fileName, codigoLayout);
+        } else if ("PF001".equals(codigoLayout)) {
+            return buildPfCarga(doc, headerEl, fileName, codigoLayout);
+        } else {
+            throw new IllegalArgumentException("Unknown layout code: " + codigoLayout);
+        }
+    }
+
+    private Element getHeaderElement(Document doc) {
         NodeList headerList = doc.getElementsByTagNameNS("*", "header");
         if (headerList.getLength() == 0) {
             throw new IllegalArgumentException("Header element not found in SIAFI XML file.");
         }
-        Element headerEl = (Element) headerList.item(0);
+        return (Element) headerList.item(0);
+    }
 
-        String codigoLayout = getTagValue(headerEl, "codigoLayout");
-        String dataGeracao = getTagValue(headerEl, "dataGeracao");
-        String sequencialGeracao = getTagValue(headerEl, "sequencialGeracao");
-        String anoReferencia = getTagValue(headerEl, "anoReferencia");
-        String ugResponsavel = getTagValue(headerEl, "ugResponsavel");
-        String cpfResponsavel = getTagValue(headerEl, "cpfResponsavel");
+    private DhCarga buildDhCarga(Document doc, Element headerEl, String fileName, String codigoLayout) {
+        DhCarga dhCarga = new DhCarga();
+        dhCarga.setFileName(fileName);
+        dhCarga.setCodigoLayout(codigoLayout);
+        dhCarga.setDataGeracao(getTagValue(headerEl, "dataGeracao"));
+        dhCarga.setSequencialGeracao(getTagValue(headerEl, "sequencialGeracao"));
+        dhCarga.setAnoReferencia(getTagValue(headerEl, "anoReferencia"));
+        dhCarga.setUgResponsavel(getTagValue(headerEl, "ugResponsavel"));
+        dhCarga.setCpfResponsavel(getTagValue(headerEl, "cpfResponsavel"));
+        dhCarga.setDataProcessamento(LocalDateTime.now());
 
-        // 2. Determine type of layout
-        if ("DH001".equals(codigoLayout)) {
-            DhCarga dhCarga = new DhCarga();
-            dhCarga.setFileName(fileName);
-            dhCarga.setCodigoLayout(codigoLayout);
-            dhCarga.setDataGeracao(dataGeracao);
-            dhCarga.setSequencialGeracao(sequencialGeracao);
-            dhCarga.setAnoReferencia(anoReferencia);
-            dhCarga.setUgResponsavel(ugResponsavel);
-            dhCarga.setCpfResponsavel(cpfResponsavel);
-            dhCarga.setDataProcessamento(LocalDateTime.now());
+        NodeList detalheList = doc.getElementsByTagNameNS("*", "detalhe");
+        dhCarga.setQuantidadeDetalhesXml(detalheList.getLength());
 
-            NodeList detalheList = doc.getElementsByTagNameNS("*", "detalhe");
-            dhCarga.setQuantidadeDetalhesXml(detalheList.getLength());
-
-            for (int i = 0; i < detalheList.getLength(); i++) {
-                Element detalheEl = (Element) detalheList.item(i);
-                NodeList dhNodes = detalheEl.getElementsByTagNameNS("*", "CprDhCadastrar");
-                if (dhNodes.getLength() > 0) {
-                    Element dhEl = (Element) dhNodes.item(0);
-                    DhDetalhe detail = parseDhDetalhe(dhEl);
-                    dhCarga.addDetalhe(detail);
-                }
+        for (int i = 0; i < detalheList.getLength(); i++) {
+            Element detalheEl = (Element) detalheList.item(i);
+            NodeList dhNodes = detalheEl.getElementsByTagNameNS("*", "CprDhCadastrar");
+            if (dhNodes.getLength() > 0) {
+                dhCarga.addDetalhe(parseDhDetalhe((Element) dhNodes.item(0)));
             }
-            return dhCarga;
-
-        } else if ("PF001".equals(codigoLayout)) {
-            PfCarga pfCarga = new PfCarga();
-            pfCarga.setFileName(fileName);
-            pfCarga.setCodigoLayout(codigoLayout);
-            pfCarga.setDataGeracao(dataGeracao);
-            pfCarga.setSequencialGeracao(sequencialGeracao);
-            pfCarga.setAnoReferencia(anoReferencia);
-            pfCarga.setUgResponsavel(ugResponsavel);
-            pfCarga.setCpfResponsavel(cpfResponsavel);
-            pfCarga.setDataProcessamento(LocalDateTime.now());
-
-            NodeList detalheList = doc.getElementsByTagNameNS("*", "detalhe");
-            pfCarga.setQuantidadeDetalhesXml(detalheList.getLength());
-
-            for (int i = 0; i < detalheList.getLength(); i++) {
-                Element detalheEl = (Element) detalheList.item(i);
-                NodeList pfNodes = detalheEl.getElementsByTagNameNS("*", "PFDTO");
-                if (pfNodes.getLength() > 0) {
-                    Element pfEl =  (Element) pfNodes.item(0);
-                    PfDetalhe detail = parsePfDetalhe(pfEl);
-                    pfCarga.addDetalhe(detail);
-                }
-            }
-            return pfCarga;
-        } else {
-            throw new IllegalArgumentException("Unknown layout code: " + codigoLayout);
         }
+        return dhCarga;
+    }
+
+    private PfCarga buildPfCarga(Document doc, Element headerEl, String fileName, String codigoLayout) {
+        PfCarga pfCarga = new PfCarga();
+        pfCarga.setFileName(fileName);
+        pfCarga.setCodigoLayout(codigoLayout);
+        pfCarga.setDataGeracao(getTagValue(headerEl, "dataGeracao"));
+        pfCarga.setSequencialGeracao(getTagValue(headerEl, "sequencialGeracao"));
+        pfCarga.setAnoReferencia(getTagValue(headerEl, "anoReferencia"));
+        pfCarga.setUgResponsavel(getTagValue(headerEl, "ugResponsavel"));
+        pfCarga.setCpfResponsavel(getTagValue(headerEl, "cpfResponsavel"));
+        pfCarga.setDataProcessamento(LocalDateTime.now());
+
+        NodeList detalheList = doc.getElementsByTagNameNS("*", "detalhe");
+        pfCarga.setQuantidadeDetalhesXml(detalheList.getLength());
+
+        for (int i = 0; i < detalheList.getLength(); i++) {
+            Element detalheEl = (Element) detalheList.item(i);
+            NodeList pfNodes = detalheEl.getElementsByTagNameNS("*", "PFDTO");
+            if (pfNodes.getLength() > 0) {
+                pfCarga.addDetalhe(parsePfDetalhe((Element) pfNodes.item(0)));
+            }
+        }
+        return pfCarga;
     }
 
     private DhDetalhe parseDhDetalhe(Element dhEl) {
@@ -102,46 +101,54 @@ public class SiafiXmlParser {
         d.setCodTipoDh(getTagValue(dhEl, "codTipoDH"));
         d.setNumDh(getTagValue(dhEl, "numDH"));
 
-        // dadosBasicos
-        NodeList basicNodes = dhEl.getElementsByTagName("dadosBasicos");
-        if (basicNodes.getLength() > 0) {
-            Element basic = (Element) basicNodes.item(0);
-            d.setDtEmis(getTagValue(basic, "dtEmis"));
-            d.setDtVenc(getTagValue(basic, "dtVenc"));
-            d.setCodUgPgto(getTagValue(basic, "codUgPgto"));
-            d.setVlr(parseBigDecimal(getTagValue(basic, "vlr")));
-            d.setTxtObser(getTagValue(basic, "txtObser"));
-            d.setTxtProcesso(getTagValue(basic, "txtProcesso"));
-            d.setDtAteste(getTagValue(basic, "dtAteste"));
-            d.setCodCredorDevedor(getTagValue(basic, "codCredorDevedor"));
-            d.setDtPgtoReceb(getTagValue(basic, "dtPgtoReceb"));
-
-            // docOrigem
-            NodeList docOrigList = basic.getElementsByTagName("docOrigem");
-            if (docOrigList.getLength() > 0) {
-                Element docOrig = (Element) docOrigList.item(0);
-                d.setDocOrigemIdentEmit(getTagValue(docOrig, "codIdentEmit"));
-                d.setDocOrigemNum(getTagValue(docOrig, "numDocOrigem"));
-                d.setDocOrigemVlr(parseBigDecimal(getTagValue(docOrig, "vlr")));
-            }
-        }
-
-        // pco (Commitment)
-        NodeList pcoNodes = dhEl.getElementsByTagName("pco");
-        if (pcoNodes.getLength() > 0) {
-            Element pco = (Element) pcoNodes.item(0);
-            d.setPcoSit(getTagValue(pco, "codSit"));
-            d.setPcoUgEmpe(getTagValue(pco, "codUgEmpe"));
-            
-            NodeList itemNodes = pco.getElementsByTagName("pcoItem");
-            if (itemNodes.getLength() > 0) {
-                Element item = (Element) itemNodes.item(0);
-                d.setPcoEmpeNum(getTagValue(item, "numEmpe"));
-                d.setPcoVlr(parseBigDecimal(getTagValue(item, "vlr")));
-            }
-        }
+        parseBasicData(dhEl, d);
+        parseCommitmentData(dhEl, d);
 
         return d;
+    }
+
+    private void parseBasicData(Element dhEl, DhDetalhe d) {
+        NodeList basicNodes = dhEl.getElementsByTagName("dadosBasicos");
+        if (basicNodes.getLength() == 0) return;
+        
+        Element basic = (Element) basicNodes.item(0);
+        d.setDtEmis(getTagValue(basic, "dtEmis"));
+        d.setDtVenc(getTagValue(basic, "dtVenc"));
+        d.setCodUgPgto(getTagValue(basic, "codUgPgto"));
+        d.setVlr(parseBigDecimal(getTagValue(basic, "vlr")));
+        d.setTxtObser(getTagValue(basic, "txtObser"));
+        d.setTxtProcesso(getTagValue(basic, "txtProcesso"));
+        d.setDtAteste(getTagValue(basic, "dtAteste"));
+        d.setCodCredorDevedor(getTagValue(basic, "codCredorDevedor"));
+        d.setDtPgtoReceb(getTagValue(basic, "dtPgtoReceb"));
+
+        parseDocOrigem(basic, d);
+    }
+
+    private void parseDocOrigem(Element basic, DhDetalhe d) {
+        NodeList docOrigList = basic.getElementsByTagName("docOrigem");
+        if (docOrigList.getLength() == 0) return;
+        
+        Element docOrig = (Element) docOrigList.item(0);
+        d.setDocOrigemIdentEmit(getTagValue(docOrig, "codIdentEmit"));
+        d.setDocOrigemNum(getTagValue(docOrig, "numDocOrigem"));
+        d.setDocOrigemVlr(parseBigDecimal(getTagValue(docOrig, "vlr")));
+    }
+
+    private void parseCommitmentData(Element dhEl, DhDetalhe d) {
+        NodeList pcoNodes = dhEl.getElementsByTagName("pco");
+        if (pcoNodes.getLength() == 0) return;
+        
+        Element pco = (Element) pcoNodes.item(0);
+        d.setPcoSit(getTagValue(pco, "codSit"));
+        d.setPcoUgEmpe(getTagValue(pco, "codUgEmpe"));
+        
+        NodeList itemNodes = pco.getElementsByTagName("pcoItem");
+        if (itemNodes.getLength() > 0) {
+            Element item = (Element) itemNodes.item(0);
+            d.setPcoEmpeNum(getTagValue(item, "numEmpe"));
+            d.setPcoVlr(parseBigDecimal(getTagValue(item, "vlr")));
+        }
     }
 
     private PfDetalhe parsePfDetalhe(Element pfEl) {
@@ -153,19 +160,22 @@ public class SiafiXmlParser {
         p.setLimiteDeSaque(Boolean.parseBoolean(getTagValue(pfEl, "limiteDeSaque")));
         p.setNumeroDocumento(getTagValue(pfEl, "numeroDocumento"));
 
-        // item (listaItemPFDTO)
-        NodeList itemNodes = pfEl.getElementsByTagName("listaItemPFDTO");
-        if (itemNodes.getLength() > 0) {
-            Element item = (Element) itemNodes.item(0);
-            p.setVlr(parseBigDecimal(getTagValue(item, "vlr")));
-            p.setCodVinc(getTagValue(item, "codVinc"));
-            p.setCodFontRecur(getTagValue(item, "codFontRecur"));
-            p.setCodCtgoGasto(getTagValue(item, "codCtgoGasto"));
-            p.setCodSit(getTagValue(item, "codSit"));
-            p.setTxtInscrA(getTagValue(item, "txtInscrA"));
-        }
+        parsePfItem(pfEl, p);
 
         return p;
+    }
+
+    private void parsePfItem(Element pfEl, PfDetalhe p) {
+        NodeList itemNodes = pfEl.getElementsByTagName("listaItemPFDTO");
+        if (itemNodes.getLength() == 0) return;
+        
+        Element item = (Element) itemNodes.item(0);
+        p.setVlr(parseBigDecimal(getTagValue(item, "vlr")));
+        p.setCodVinc(getTagValue(item, "codVinc"));
+        p.setCodFontRecur(getTagValue(item, "codFontRecur"));
+        p.setCodCtgoGasto(getTagValue(item, "codCtgoGasto"));
+        p.setCodSit(getTagValue(item, "codSit"));
+        p.setTxtInscrA(getTagValue(item, "txtInscrA"));
     }
 
     private String getTagValue(Element element, String tagName) {
